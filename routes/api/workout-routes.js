e84572c584b165e94a6dcc28e6dd412063f40b9b
Samuel Heal <samuelheal@hotmail.com>
2021-07-05 16:20:17 +1000

    workout get routes added

--- routes/api/workout-routes.js
+++ routes/api/workout-routes.js
@@ -1,8 +1,40 @@
 const router = require('express').Router();
 const db = require('../../models');
 
+router.get("/", (req, res) => {
+    db.Workout.aggregate([
+        {
+          $addFields: {
+            totalDuration: { $sum: "$exercises.duration"},
+          },
+        },
+      ])
+      .then((workoutDB) => {
+        res.json(workoutDB);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
 
+router.get("/range", (req, res) => {
+    db.Workout.aggregate([
+        {
+          $addFields: {
+            totalDuration: { $sum: "$exercises.duration"},
+          },
+        },
+      ])
+      .limit(7)
+      .then(workoutDB => {
+        res.json(workoutDB);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
 
+  
 
 
 module.exports = router;
